027ba1b34a2107706590e3b58b62a3b3602ecc15
ryutaroM <arcticmonkey326@gmail.com>
2025-11-10 23:53:13 +0900

    countcharとcountdownの新規ファイルを追加

---
+++ chap3/countdown/countdown.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	count := 5
+	go countdown(&count)
+	for count > 0 {
+		time.Sleep(500 * time.Microsecond)
+		fmt.Println(count)
+	}
+}
+
+func countdown(seconds *int) {
+	for *seconds > 0 {
+		time.Sleep(1 * time.Second)
+		*seconds -= 1
+	}
+}
