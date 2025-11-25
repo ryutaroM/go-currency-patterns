9f2a249ac694703ee238c0fa6081a9d232f0de37
ryutaroM <arcticmonkey326@gmail.com>
2025-11-26 00:06:13 +0900

    feat: add simple mutex implementation for match event handling and RW mutex version implementation

---
+++ chap4/gameplaymutex/readwritermutex/readwritermutex.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	mutex := sync.RWMutex{}
+	var matchEvents = make([]string, 0, 10000)
+	for j := 0; j < 10000; j++ {
+		matchEvents = append(matchEvents, "Init Match Event")
+	}
+	go matchRecorder(&matchEvents, &mutex)
+	start := time.Now()
+	for j := 0; j < 5000; j++ {
+		go clientHandler(&matchEvents, &mutex, start)
+	}
+	time.Sleep(100 * time.Second)
+}
+
+func matchRecorder(matchEvents *[]string, mutex *sync.RWMutex) {
+	for i := 0; ; i++ {
+		mutex.Lock()
+		*matchEvents = append(*matchEvents, "Match Event "+string(i))
+		mutex.Unlock()
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println("Appended match event")
+	}
+}
+
+func clientHandler(mEvents *[]string, mutex *sync.RWMutex, st time.Time) {
+	for i := 0; i < 100; i++ {
+		mutex.RLock()
+		allEvents := copyAllEvents(mEvents)
+		mutex.RUnlock()
+		timeTaken := time.Since(st)
+		fmt.Println(len(allEvents), "events copied in", timeTaken)
+	}
+}
+
+func copyAllEvents(matchEvents *[]string) []string {
+	allEvents := make([]string, 0, len(*matchEvents))
+	for _, e := range *matchEvents {
+		allEvents = append(allEvents, e)
+	}
+	return allEvents
+}
