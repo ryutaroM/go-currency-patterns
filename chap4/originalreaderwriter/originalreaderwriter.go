5185086c0c5d1322a3c00b969eabbf5878850f03
ryutaroM <arcticmonkey326@gmail.com>
2025-12-02 00:14:12 +0900

    feat: add original reader writer mutex

---
+++ chap4/originalreaderwriter/originalreaderwriter.go
@@ -0,0 +1,35 @@
+package originalreaderwriter
+
+import "sync"
+
+type ReaderWriterMutex struct {
+	readersCounter int
+	readersLock    sync.Mutex
+	globalLock     sync.Mutex
+}
+
+func (rw *ReaderWriterMutex) ReadLock() {
+	rw.readersLock.Lock()
+	rw.readersCounter++
+	if rw.readersCounter == 1 {
+		rw.globalLock.Lock() //block writers
+	}
+	rw.readersLock.Unlock()
+}
+
+func (rw *ReaderWriterMutex) ReadUnlock() {
+	rw.readersLock.Lock()
+	rw.readersCounter--
+	if rw.readersCounter == 0 {
+		rw.globalLock.Unlock() //unblock writers
+	}
+	rw.readersLock.Unlock()
+}
+
+func (rw *ReaderWriterMutex) WriteLock() {
+	rw.globalLock.Lock() // block readers and writers
+}
+
+func (rw *ReaderWriterMutex) WriteUnlock() {
+	rw.globalLock.Unlock()
+}
