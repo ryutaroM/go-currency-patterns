3b0ebb6965d91180fb98ed2ae79e656b27ae2b7d
ryutaroM <arcticmonkey326@gmail.com>
2025-12-22 22:32:54 +0900

    feat: implement read and write unlock methods for ReadWriteMutex

--- chap5/writepreference/main.go
+++ chap5/writepreference/main.go
@@ -1,6 +1,28 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	rwMutex := NewReadWriteMutex()
+	for i := 0; i < 2; i++ {
+		go func() {
+			for {
+				rwMutex.ReadLock()
+				time.Sleep(1 * time.Second)
+				fmt.Println("Read done")
+				rwMutex.ReadUnlock()
+			}
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	rwMutex.WriteLock()
+	fmt.Println("Write finished")
+
+}
 
 type ReadWriteMutex struct {
 	readersCounter int
@@ -18,7 +40,16 @@ func (rw *ReadWriteMutex) ReadLock() {
 	rw.cond.L.Unlock()
 }
 
-func (rw *ReadWriteMutex) WriterLock() {
+func (rw *ReadWriteMutex) ReadUnlock() {
+	rw.cond.L.Lock()
+	rw.readersCounter--
+	if rw.readersCounter == 0 {
+		rw.cond.Broadcast()
+	}
+	rw.cond.L.Unlock()
+}
+
+func (rw *ReadWriteMutex) WriteLock() {
 	rw.cond.L.Lock()
 
 	rw.writersWaiting++
@@ -28,6 +59,13 @@ func (rw *ReadWriteMutex) WriterLock() {
 	rw.writersWaiting--
 	rw.writerActive = true
 
+	rw.cond.L.Unlock()
+}
+
+func (rw *ReadWriteMutex) WriteUnlock() {
+	rw.cond.L.Lock()
+	rw.writerActive = false
+	rw.cond.Broadcast()
 	rw.cond.L.Unlock()
 }
 
