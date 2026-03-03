22e0460ddcc0dcb6aebb6533e0e4d6c92f071917
ryutaroM <arcticmonkey326@gmail.com>
2026-03-04 00:02:51 +0900

    chore: add receive method

--- chap7/7.14/main.go
+++ chap7/7.14/main.go
@@ -31,3 +31,15 @@ func (c *Channel[M]) Send(message M) {
 
 	c.sizeSema.Release()
 }
+
+func (c *Channel[M]) Receive() M {
+	c.capacitySema.Release()
+
+	c.sizeSema.Acquire()
+
+	c.mutex.Lock()
+	v := c.buffer.Remove(c.buffer.Front()).(M)
+	c.mutex.Unlock()
+
+	return v
+}
