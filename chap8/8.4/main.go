66219029001e696044f89af88e4154227fc65205
ryutaroM <arcticmonkey326@gmail.com>
2026-03-16 23:51:42 +0900

    feat: 8.5,6

--- chap8/8.4/main.go
+++ chap8/8.4/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 const (
 	passwordGuess = "go far"
 	alpahabet     = "abcdefghijklmnopqrstuvwxyz "
@@ -12,4 +17,33 @@ func toBase27(n int) string {
 		n /= 27
 	}
 	return result
+}
+
+func guessPassword(from int, upto int, stop chan int, result chan string) {
+	for guessN := from; guessN < upto; guessN++ {
+		select {
+		case <-stop:
+			fmt.Printf("Stopped at %d [%d, %d]\n", guessN, from, upto)
+			return
+		default:
+			if toBase27(guessN) == passwordGuess {
+				result <- toBase27(guessN)
+				close(stop)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	finished := make(chan int)
+	passwordFound := make(chan string)
+
+	for i := 1; i <= 387_420_488; i += 10_000_000 {
+		go guessPassword(i, i+10_000_000, finished, passwordFound)
+	}
+
+	fmt.Println("password found:", <-passwordFound)
+	close(passwordFound)
+	time.Sleep(5 * time.Second)
 }
